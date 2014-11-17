6e413044f6b8465333a2803f8b7d42337e8a08e9
Maillard Mathieu <maillard@turing.u-strasbg.fr>
2014-11-17 16:58:50 +0100

    init ssh

---
+++ system.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+echo "Systeme d'exploitation : `cat /proc/sys/kernel/ostype`"
+echo "Version du noyau : `cat /proc/sys/kernel/osrelease`"
+
+UP=`cat /proc/uptime | cut -d ' ' -f 1 | cut -d '.' -f 1` # Je ne prend que la premiere valeur renvoyee par /proc/uptime puis je garde uniquement la partie entiere de la valeur retournee.
+UP=`expr $UP / 3600 / 24`
+
+echo "Temps ecoule depuis le dernier redemarrage : $UP jours"
+
+echo "Utilisation globale moyenne CPU (5 min/10 min/15 min) : `cat /proc/loadavg | cut -d ' ' -f 1-3`"
+
+NBUSERS=`who | wc -l`
+if [ $NBUSERS -eq 1 ]
+then
+	echo "Il y a actuellement $NBUSERS utilisateur connecte (vous)"
+else
+	echo "Il y a actuellement $NBUSERS utilisateurs connectes"
+fi
