43b06c0382d9618b1cf62f052ffe03230fd57c6d
Geoffroy Elliott <elliott.geoffroy@turing.u-strasbg.fr>
2014-11-18 18:56:09 +0100

    MEDIA de A MAXIMA

--- system.sh
+++ system.sh
@@ -36,6 +36,7 @@ find . -type f 2>/dev/null | xargs du -b 2>/dev/null | sort -n -r | head -5
 
 #A MEDIA
 
+#PARTIE PROCESSUS
 echo "nombre de processus desire ?"
 read prcss
 echo $prcss | grep -E "^[0-9]+$" 1>/dev/null
@@ -51,10 +52,12 @@ echo "les $prcss processus ayant consomme le plus de temps CPU depuis leur lance
 echo "PID\tnom\t\ttics"
 cat /proc/[0-9]*/stat | cut -d ' ' -f  1,2,15 | sort -t " " -k 3 -n -r | head -"$prcss" | tr " " "\t"
 
+
+#PARTIE DISK UTIL.
 echo "nombre de fichier desire ?"
 read prcss
 echo $prcss | grep -E "^[0-9]+$" 1>/dev/null
-while [ $? != 0 ]
+while [ $? -ne 0 ]
 do
 	echo "nombre de fichier desire ? (il faut entrer un nombre)"
 	read prcss
@@ -62,6 +65,92 @@ do
 	echo $prcss | grep -E "^[0-9]+$" 1>/dev/null
 done
 
+echo "Voulez vous changer de repertoire courant ? (y/n)"
+read yn
+if echo $yn | grep "y" 1>/dev/null
+then
+	boo=0
+	while [ $boo -eq 0 ]
+	do
+		echo "Entrez le repertoire souhaite (chemin relatif):"
+		read chemin
+			if echo $chemin | grep -E "^[.]{1,2}[/].*$" 1>/dev/null
+			then : 
+			else
+				chemin="./""$chemin" 
+			fi
+		if [ -d $chemin ]
+		then
+			find "$chemin" -type f 2>/dev/null | xargs du -b 2>/dev/null | sort -n -r | head -"$prcss"
+			boo=1
+		else
+			echo "repertoire \"$chemin\" inexistant" >&2	
+		fi 
+	done
+else
 echo "les $prcss fichiers (incluant les répertoires) occupant le plus d’espace disque"
 echo "taille (Octets)\tnom"
 find . -type f 2>/dev/null | xargs du -b 2>/dev/null | sort -n -r | head -"$prcss"
+
+fi
+
+#PARTIE KILL PROCESS
+
+echo "Voulez-vous kill un processus ? (y/n)"
+
+read yn
+if echo $yn | grep "y" 1>/dev/null
+then
+	boo=0
+	while [ $boo -eq 0 ]
+	do
+		echo "Entrez le PID du processus :"
+		read killer
+		kill $killer
+		if [ $? -ne 0 ]
+		then
+			echo "Le processus n'a pas pu etre supprime (en etes-vous le proprietaire ?)"
+		else
+			echo "processus detruit"		
+		fi
+		
+		echo "kill un autre processus ? (y/n)"
+		read yn
+		if echo $yn | grep "y" 1>/dev/null
+		then
+			boo=0
+		else
+			boo=1
+		fi
+	done
+fi
+
+#A MAXIMA
+
+#nice : priorité 19
+
+# ls -dl /proc/18054  Starttime
+
+# cat /proc/[pid]/status -> VmRSS (Kb)
+
+FIN=`date -u +%s --date="2014-11-18 18:45"`
+ echo $FIN
+DEBUT=`date -u +%s --date="2014-10-24 19:35"`
+echo $DEBUT
+echo $(($FIN - $DEBUT))
+
+INTERVALLE=`echo $(($FIN - $DEBUT))`
+
+INTERVALLE=$(($INTERVALLE / 60))    # intervalle exprimé en minutes
+echo $INTERVALLE
+minutes=$(($INTERVALLE % 60))       # le reste d'une division par 60
+echo $minutes
+INTERVALLE=$(( ($INTERVALLE - $minutes)/60 )) # on retire le surplus et on
+                                                  # divise par 60 pour avoir
+                                                 # l'intervalle en heures 
+echo $INTERVALLE
+heures=$(($INTERVALLE % 24))        # idem pour les heures
+echo $heures                                    
+INTERVALLE=$(( ($INTERVALLE - $heures)/24 ))  # intervalle expr. en jours 
+echo $INTERVALLE
+echo "$INTERVALLE jours, $heures heures et $minutes minutes."
