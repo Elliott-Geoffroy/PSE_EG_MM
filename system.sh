6a3258e25aca4c111a66315324e0df344091d626
Geoffroy Elliott <elliott.geoffroy@turing.u-strasbg.fr>
2014-11-18 16:50:14 +0100

    A MEDIA BEGINNING

--- system.sh
+++ system.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+#A MINIMA
+
 echo "Systeme d'exploitation : `cat /proc/sys/kernel/ostype`"
 echo "Version du noyau : `cat /proc/sys/kernel/osrelease`"
 
@@ -24,8 +26,42 @@ else
 	echo "Il y a actuellement $NBUSERS utilisateurs connectes"
 fi
 
-echo "les 5 processus ayant consommé le plus de temps CPU depuis leur lancement :"
+echo "les 5 processus ayant consomme le plus de temps CPU depuis leur lancement :"
 echo "PID\tnom\t\ttics"
 cat /proc/[0-9]*/stat | cut -d ' ' -f  1,2,15 | sort -t " " -k 3 -n -r | head -5 | tr " " "\t"
 
-find . -type f | xargs du -b 2>/dev/null | sort -n | tail -5 
+echo "les 5 fichiers (incluant les répertoires) occupant le plus d’espace disque"
+echo "taille (Octets)\tnom"
+find . -type f 2>/dev/null | xargs du -b 2>/dev/null | sort -n -r | head -5
+
+#A MEDIA
+
+echo "nombre de processus desire ?"
+read prcss
+echo $prcss | grep -E "^[0-9]+$" 1>/dev/null
+while [ $? != 0 ]
+do
+	echo "nombre de processus desire ? (il faut entrer un nombre)"
+	read prcss
+	
+	echo $prcss | grep -E "^[0-9]+$" 1>/dev/null
+done
+
+echo "les $prcss processus ayant consomme le plus de temps CPU depuis leur lancement :"
+echo "PID\tnom\t\ttics"
+cat /proc/[0-9]*/stat | cut -d ' ' -f  1,2,15 | sort -t " " -k 3 -n -r | head -"$prcss" | tr " " "\t"
+
+echo "nombre de fichier desire ?"
+read prcss
+echo $prcss | grep -E "^[0-9]+$" 1>/dev/null
+while [ $? != 0 ]
+do
+	echo "nombre de fichier desire ? (il faut entrer un nombre)"
+	read prcss
+	
+	echo $prcss | grep -E "^[0-9]+$" 1>/dev/null
+done
+
+echo "les $prcss fichiers (incluant les répertoires) occupant le plus d’espace disque"
+echo "taille (Octets)\tnom"
+find . -type f 2>/dev/null | xargs du -b 2>/dev/null | sort -n -r | head -"$prcss"
