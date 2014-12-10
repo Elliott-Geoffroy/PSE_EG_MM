b6ea62f033039213c94d4914112ca837336426d4
elliott-geoffroy <elliott.geoffroy@gmail.com>
2014-12-10 04:15:54 +0100

    beqshre

--- Elliott_Geoffroy.asm
+++ Elliott_Geoffroy.asm
@@ -542,55 +542,73 @@ sub 	$sp, $sp, 4
 	jal carreNValide
 	lw $a1, varcol
 	add $a1, $a1 , $v1
-	
+	move $a0 $v1
+	li $v0,  1
+	syscall
 	li $a3, 1
 	sw $a1, varcol
 	jal carreNValide
 	lw $a1, varcol
 	add $a1, $a1 , $v1
-	
+		move $a0 $v1
+	li $v0,  1
+	syscall
 	li $a3, 2
 	sw $a1, varcol
 	jal carreNValide
 	lw $a1, varcol
 	add $a1, $a1 , $v1
-	
+		move $a0 $v1
+	li $v0,  1
+	syscall
 	li $a3, 3
 	sw $a1, varcol
 	jal carreNValide
 	lw $a1, varcol
 	add $a1, $a1 , $v1
-	
+		move $a0 $v1
+	li $v0,  1
+	syscall
 	li $a3, 4
 	sw $a1, varcol
 	jal carreNValide
 	lw $a1, varcol
 	add $a1, $a1 , $v1
-	
+		move $a0 $v1
+	li $v0,  1
+	syscall
 	li $a3, 5
 	sw $a1, varcol
 	jal carreNValide
 	lw $a1, varcol
 	add $a1, $a1 , $v1
-	
+		move $a0 $v1
+	li $v0,  1
+	syscall
 	li $a3, 6
 	sw $a1, varcol
 	jal carreNValide
 	lw $a1, varcol
 	add $a1, $a1 , $v1
-	
+		move $a0 $v1
+	li $v0,  1
+	syscall
 	li $a3, 7
 	sw $a1, varcol
 	jal carreNValide
 	lw $a1, varcol
 	add $a1, $a1 , $v1
-	
+		move $a0 $v1
+	li $v0,  1
+	syscall
 	li $a3, 8
 	sw $a1, varcol
 	jal carreNValide
 	lw $a1, varcol
 	add $a1, $a1 , $v1
-	
+		move $a0 $v1
+	li $v0,  1
+	syscall
 	
 
 	bne $a1, 9, allcarFalse
