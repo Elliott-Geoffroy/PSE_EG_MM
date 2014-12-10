955ab01ccc6a7d71b01f822b2cc0a030848b115e
elliott-geoffroy <elliott.geoffroy@gmail.com>
2014-12-10 03:15:43 +0100

    fwdfd

--- Elliott_Geoffroy.asm
+++ Elliott_Geoffroy.asm
@@ -375,17 +375,28 @@ sub 	$sp, $sp, 4
 	loop_all_colonnes:
 		jal colonneNValide
 		add $a1, $a1, $v1
+		move $a0 $v1
+		li $v0,  1
+		syscall
+		
 		beq $a3, 8, end_loop_all_colonnes
 		add $a3, $a3, 1
 	j loop_all_colonnes
 	end_loop_all_colonnes:
-
+	
+	jal newLine
+	
+	move $a0 $a1
+	li $v0,  1
+	syscall
+	
 	bne $a1, 9, allColFalse
 		li 	$v1, 1 #carre OK (TRUE)
 		j out_allCol_val
 	allColFalse:
 		li 	$v1, 0 #carre NOT OK (FALSE)
 	out_allCol_val:	
+	jal newLine
 	
 	lw 		$ra, 0($sp)
 	add 	$sp, $sp, 4
