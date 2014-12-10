7a6bd442cce6394da1351bea8277b720aa2ecdbb
elliott-geoffroy <elliott.geoffroy@gmail.com>
2014-12-10 03:56:23 +0100

    gsgsgbsz

--- Elliott_Geoffroy.asm
+++ Elliott_Geoffroy.asm
@@ -524,38 +524,7 @@ main:
 	#syscall
 	#jal newLine
 
-	li $a3 0
-	sw $a1, varcol
-	jal colonneNValide
-	lw $a1, varcol
-	add $a1, $a1 , $v1
-	move $a0 $v1
-	li $v0,  1
-	syscall
-	li $a3 1
-	sw $a1, varcol
-	jal colonneNValide
-	lw $a1, varcol
-	add $a1, $a1 , $v1
-	move $a0 $v1
-	li $v0,  1
-	syscall
-	li $a3 2
-	sw $a1, varcol
-	jal colonneNValide
-	lw $a1, varcol
-	add $a1, $a1 , $v1
-	move $a0 $v1
-	li $v0,  1
-	syscall
-	li $a3 3
-	sw $a1, varcol
-	jal colonneNValide
-	lw $a1, varcol
-	add $a1, $a1 , $v1
-	move $a0 $v1
-	li $v0,  1
-	syscall
+	
 	li $a3 4
 	sw $a1, varcol
 	jal colonneNValide
@@ -564,38 +533,7 @@ main:
 	move $a0 $v1
 	li $v0,  1
 	syscall
-	li $a3 5
-	sw $a1, varcol
-	jal colonneNValide
-	lw $a1, varcol
-	add $a1, $a1 , $v1
-	move $a0 $v1
-	li $v0,  1
-	syscall
-	li $a3 6
-	sw $a1, varcol
-	jal colonneNValide
-	lw $a1, varcol
-	add $a1, $a1 , $v1
-	move $a0 $v1
-	li $v0,  1
-	syscall
-	li $a3 7
-	sw $a1, varcol
-	jal colonneNValide
-	lw $a1, varcol
-	add $a1, $a1 , $v1
-	move $a0 $v1
-	li $v0,  1
-	syscall
-	li $a3 8
-	sw $a1, varcol
-	jal colonneNValide
-	lw $a1, varcol
-	add $a1, $a1 , $v1
-	move $a0 $v1
-	li $v0,  1
-	syscall
+	
 
 # Fin de la zone d'appel de fonctions.
 jal newLine
