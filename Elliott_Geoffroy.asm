58241d151d702c41e16f3357239c130ea6e3a8e1
elliott-geoffroy <elliott.geoffroy@gmail.com>
2014-12-10 03:58:03 +0100

    bdesbebdebedb

--- Elliott_Geoffroy.asm
+++ Elliott_Geoffroy.asm
@@ -424,14 +424,7 @@ sub 	$sp, $sp, 4
 	move $a0 $v1
 	li $v0,  1
 	syscall
-	li $a3 4
-	sw $a1, varcol
-	jal colonneNValide
-	lw $a1, varcol
-	add $a1, $a1 , $v1
-	move $a0 $v1
-	li $v0,  1
-	syscall
+	
 	li $a3 5
 	sw $a1, varcol
 	jal colonneNValide
@@ -464,7 +457,14 @@ sub 	$sp, $sp, 4
 	move $a0 $v1
 	li $v0,  1
 	syscall
-	
+	li $a3 4
+	sw $a1, varcol
+	jal colonneNValide
+	lw $a1, varcol
+	add $a1, $a1 , $v1
+	move $a0 $v1
+	li $v0,  1
+	syscall
 
 	bne $a1, 9, allColFalse
 		li 	$v1, 1 #carre OK (TRUE)
@@ -524,80 +524,7 @@ main:
 	#syscall
 	#jal newLine
 
-	li $a3 0
-	sw $a1, varcol
-	jal colonneNValide
-	lw $a1, varcol
-	add $a1, $a1 , $v1
-	move $a0 $v1
-	li $v0,  1
-	syscall
-	li $a3 1
-	sw $a1, varcol
-	jal colonneNValide
-	lw $a1, varcol
-	add $a1, $a1 , $v1
-	move $a0 $v1
-	li $v0,  1
-	syscall
-	li $a3 2
-	sw $a1, varcol
-	jal colonneNValide
-	lw $a1, varcol
-	add $a1, $a1 , $v1
-	move $a0 $v1
-	li $v0,  1
-	syscall
-	li $a3 3
-	sw $a1, varcol
-	jal colonneNValide
-	lw $a1, varcol
-	add $a1, $a1 , $v1
-	move $a0 $v1
-	li $v0,  1
-	syscall
 	
-	li $a3 5
-	sw $a1, varcol
-	jal colonneNValide
-	lw $a1, varcol
-	add $a1, $a1 , $v1
-	move $a0 $v1
-	li $v0,  1
-	syscall
-	li $a3 6
-	sw $a1, varcol
-	jal colonneNValide
-	lw $a1, varcol
-	add $a1, $a1 , $v1
-	move $a0 $v1
-	li $v0,  1
-	syscall
-	li $a3 7
-	sw $a1, varcol
-	jal colonneNValide
-	lw $a1, varcol
-	add $a1, $a1 , $v1
-	move $a0 $v1
-	li $v0,  1
-	syscall
-	li $a3 8
-	sw $a1, varcol
-	jal colonneNValide
-	lw $a1, varcol
-	add $a1, $a1 , $v1
-	move $a0 $v1
-	li $v0,  1
-	syscall
-	
-	li $a3 4
-	sw $a1, varcol
-	jal colonneNValide
-	lw $a1, varcol
-	add $a1, $a1 , $v1
-	move $a0 $v1
-	li $v0,  1
-	syscall
 
 # Fin de la zone d'appel de fonctions.
 jal newLine
